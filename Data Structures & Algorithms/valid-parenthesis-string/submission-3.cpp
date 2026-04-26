d642777cddc30ad28394be02b57ecb67ca87579a
Tiya <tiyasinghdulce@gmail.com>
2026-04-26 23:38:46 +0530

    Add: valid-parenthesis-string - submission-3

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int leftmin=0,leftmax=0;
+        for(const auto& i:s){
+            if(i=='('){
+                leftmin++;
+                leftmax++;
+            } else if(i==')'){
+                leftmin--;
+                leftmax--;
+            } else{
+                leftmin--;
+                leftmax++;
+            }
+            if(leftmax<0) return false;
+            if(leftmin<0) leftmin=0;
+        }
+        return leftmin==0;
+    }
+};
